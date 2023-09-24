2348e61219f274b508f7f30a002cc23f357494ba
Ankit Agrawal <ankitagrawal1881@gmail.com>
2023-09-24 14:53:42 +0530

    project setup complete

---
+++ src/app/consultancy-fees/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ConsultancyFees = () => {
+  return <div>ConsultancyFees</div>;
+};
+
+export default ConsultancyFees;
---
+++ src/app/gau-sewa/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GauSewa = () => {
+  return <div>GauSewa</div>;
+};
+
+export default GauSewa;
