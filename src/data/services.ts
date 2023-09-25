1a08e56732cb497924309a1db40e343107746be0
Gaurav <grvpande@gmail.com>
2023-09-25 13:52:27 +0530

    adding files for store

---
+++ src/data/services.ts
@@ -0,0 +1,73 @@
+export const services = [
+    {
+        id: 1,
+        image: "/images/ONLINE-PREDICTION.webp",
+        title: "Online Prediction",
+        cost: "3000",
+        description:
+            "Horoscopes that we provide as per Vedic Astrology. Horoscope reflects your past, present and future life, and helps to unlock challenges, decipher opportunities and chances coming your way. It also provide daily, weekly, monthly and yearly guidance through predictions.",
+    },
+    {
+        id: 2,
+        image: "/images/WHOLE-LIFE-PREDICTION-15-Y.webp",
+        title: "Whole Life Prediction (15 Yr)",
+        cost: "21000",
+        description:
+            "With in depth analysis of complete life, get answers to all questions like what, when, how and why a particular event will occur in life. Get an insight on your personal, professional, financial potential and core competence.",
+    },
+    {
+        id: 3,
+        image: "/images/kundali-matching.webp",
+        title: "Kundali / Match Making",
+        cost: "2100",
+        description:
+            "Kundli Milan is also called ‘Gun Milan’. Kundli matching help you find a way to your partner and lead a happy and prosperous married life. Along with Gun Milan, checkout other important aspects such as Nadi Dosha, Manglik Dosha, Gana Dosha and much more.",
+    },
+    {
+        id: 4,
+        image: "/images/HORARY-astrology3.webp",
+        title: "Horary Astrology",
+        cost: "1500",
+        description:
+            "Horary Astrology is also called 'Prasna Sastra' in India, because it answers specific queries (prasna) depending on the time of the query. Horary Astrology focuses mainly on answering specific questions. Horary Astrology focuses mainly on answering specific questions.",
+    },
+    {
+        id: 5,
+        image: "/images/Medical-Astrology.webp",
+        title: "Medical Astrology",
+        cost: "3000",
+        description:
+            "Medical astrology (traditionally known as Iatromathematics) is an ancient medical system that associates various parts of the body, diseases, and drugs as under the influence of the sun, moon, and planets, along with the twelve astrological signs.",
+    },
+    {
+        id: 6,
+        image: "/images/MUNDANE-ASTROLOGY2.webp",
+        title: "Mundane Astrology",
+        cost: "3000",
+        description:
+            "Mundane Astrology is also called 'Medhini Jyothisha' or 'Desa Gochara' in Vedic culture. It is also referred to as 'Political Astrology' by modern Astrologers. Mundane astrology used to predict world affairs and events.",
+    },
+    {
+        id: 7,
+        image: "/images/EDUCATION-PREDICTION2.webp",
+        title: "Education / Career Prediction",
+        cost: "2100",
+        description:
+            "From the perspective of astrology it can also point out and hint at the periods best suitable for your education. Choose the right stream of education to choose a profession/career. Provide the right education to your child according to the favourable planetary positions in his or her horoscope to avoid any issues at a later stage.",
+    },
+    {
+        id: 8,
+        image: "/images/Career-Horoscope.webp",
+        title: "Finance / Business Prediction",
+        cost: "3000",
+        description:
+            "A good job or a successful business is required for survival and for living a comfortable life. If you are planning to start up a career or business, then you can counsult Pt. Govind Pandey ji to know how things would shape up in the future or to know what do you need to do in order to be successful in life.",
+    },
+    {
+        id: 9,
+        image: "/images/01.webp",
+        title: "Gems Consultation",
+        cost: "1000",
+        description: "",
+    },
+];
