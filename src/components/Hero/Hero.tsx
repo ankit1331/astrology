528ad4c59beece06d1aea2f401a88e30c1db31c4
Gaurav <grvpande@gmail.com>
2023-10-10 13:09:39 +0530

    header

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return <div>
+    <div className='container'>
+      <div className="row">
+        <div className="col-12 col-sm-6">Image</div>
+        <div className="col-12 col-sm-6">
+          <h2>Welcome to the Jageshwar astro consultants.</h2>
+        </div>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Hero;
