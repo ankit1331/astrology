1a0c126330254612535c1d4c2ce43ff81d6a8566
Ankit Agrawal <ankitagrawal1881@gmail.com>
2023-10-10 15:51:17 +0530

    scss structure changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <nav
-      id='pr-nav'
-      className='primary-menu navbar navbar-expand-lg navbar-dark'
-    >
-      
+    <nav id='pr-nav' className='nav-bar'>
+      <div className='logo'>
+        <a href='#'>
+          <img src='/images/logo.png' alt='logo' />
+        </a>
+      </div>
+      <div className='menu-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Services</li>
+          <li>Consultancy Fees</li>
+          <li>Gau Sewa</li>
+          <li>Payments</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
     </nav>
   );
 };
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>
-    <div className='container'>
-      <div className="row">
-        <div className="col-12 col-sm-6">Image</div>
-        <div className="col-12 col-sm-6">
-          <h2>Welcome to the Jageshwar astro consultants.</h2>
+  return (
+    <div className='hero-bg'>
+      <div className='hero-bg-image'></div>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-12 col-sm-6'>Image</div>
+          <div className='col-12 col-sm-6'>
+            <h2>Welcome to the Jageshwar astro consultants.</h2>
+            <button>Test</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 };
 
 export default Hero;
